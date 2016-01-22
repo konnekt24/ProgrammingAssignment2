e7829165291a8547d2f0a9b48b4ef6a785bbdd6d
konnekt24 <konnekt24@gmail.com>
2016-01-22 17:11:58 +0900

    Assignment
    How it works:
    a <- makeCacheMatrix() ##it will make empty matrix.
    a$set(matrix(element data, nrow  =, ncol =) ##you can put any data. And
    you must put same number on nrow and ncol arg.

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+
+#After making empty matrix, put the element of matrix placed thru 'set'       
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+
+        get <- function() x
+        solvematrix <- function(solve) m <<- solve
+        getmatrix <- function() m
+        list(set = set, get = get, setmatrix = setmatrix, getmatrix = getmatrix)
 
 }
 
@@ -12,4 +24,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+       m <- x$getmatrix()
+## now checking the given matrix whether it's NULL or not.
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        matrix <- x$get() 
+## solve() function returns an inverse matrix of given one, if it is a
+## square matrix. or it will print the "U[nrow, ncol] = 0"
+        m <- solve(matrix, ...)
+        x$solvematrix(m)
+        m
 }
